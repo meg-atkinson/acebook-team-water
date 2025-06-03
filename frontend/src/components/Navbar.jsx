adcf463d20dcfe3bfc1f7f5febc131c45b0e03d1
evemoore <eveiaim98@outlook.com>
2025-06-03 15:48:44 +0100

    Fixed navbar styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,15 +6,19 @@ import logo from '../assets/acebook.svg';
 function Navbar() {
   return (
     <nav className="navbar">
-        <div className="navbar-left">
-            <img src={logo} alt="Logo" className="logo" />
-        </div>
-      <ul className="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">Profile</a></li>
-        <li><a href="/contact">Friends</a></li>
-        <li><a href="/logout">Log Out</a></li>
-      </ul>
+      <div className="navbar-left">
+        <img src={logo} alt="Logo" className="logo" />
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">Profile</a></li>
+          <li><a href="/contact">Friends</a></li>
+        </ul>
+      </div>
+      <div className='navbar-right'>
+        <ul>
+          <li><a href="/logout">Log Out</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
