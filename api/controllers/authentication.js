17f577041d1ae94f371230a6f76191c655d7630d
George <georgeatkinsonwd@gmail.com>
2025-06-04 14:55:46 +0100

    added hashed passwords and check for login

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -1,22 +1,38 @@
 const User = require("../models/user");
 const { generateToken } = require("../lib/token");
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 async function createToken(req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
+try {
   const user = await User.findOne({ email: email });
+
   if (!user) {
     console.log("Auth Error: User not found");
     res.status(401).json({ message: "User not found" });
-  } else if (user.password !== password) {
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
     console.log("Auth Error: Passwords do not match");
-    res.status(401).json({ message: "Password incorrect" });
-  } else {
+    return res.status(401).json({ message: "Password incorrect" });
+    }
+
     const token = generateToken(user.id);
     res.status(201).json({ token: token, message: "OK" });
-  }
+
+} 
+catch (err) {
+  console.error("Login error:", err);
+  res.status(500).json({ message: "Internal server error" })
 }
+}
+  
+
 
 const AuthenticationController = {
   createToken: createToken,
--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,14 +1,21 @@
 const User = require("../models/user");
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 function create(req, res) {
   const email = req.body.email;
   const password = req.body.password;
   const basicInfo = req.body.basicInfo;
 
+ bcrypt.hash(password, saltRounds, function(err, hash) {
+   if (err) {
+      return res.status(500).json({ error: "Error hashing password" });
+    }
+
   const user = new User({
-  email, 
-  password,
-  basicInfo,
+  email: email, 
+  password: hash,
+  basicInfo: basicInfo,
 });
 
   user
@@ -20,7 +27,10 @@ function create(req, res) {
     .catch((err) => {
       console.error(err);
       res.status(400).json({ message: "Something went wrong" });
-    });
+    }); 
+});
+
+  
 }
 
 const UsersController = {
