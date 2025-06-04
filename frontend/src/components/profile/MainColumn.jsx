f54506e45415c26b5709a9eb22a4d771ac715748
evemoore <eveiaim98@outlook.com>
2025-06-04 13:38:57 +0100

    Created photos tab

--- frontend/src/components/profile/MainColumn.jsx
+++ frontend/src/components/profile/MainColumn.jsx
@@ -3,16 +3,19 @@ import { useState } from "react"
 import { TabBar } from "./TabBar"
 import { Wall } from "./Wall"
 import { Info } from "./Info";
+import { Photos } from "./Photos";
 
 export const MainColumn = () => {
     const [showWall, setShowWall] = useState(false);
     const [showInfo, setShowInfo] = useState(false);
+    const [showPhotos, setShowPhotos] = useState(false);
 
     return (
     <div className="mainColumn">
-        <TabBar showWall={showWall} setShowWall={setShowWall} showInfo={showInfo} setShowInfo={setShowInfo}/>
+        <TabBar showWall={showWall} setShowWall={setShowWall} showInfo={showInfo} setShowInfo={setShowInfo} showPhotos={showPhotos} setShowPhotos={setShowPhotos} />
         <Wall showWall={showWall} />
         <Info showInfo={showInfo} />
+        <Photos showPhotos={showPhotos}/>
     </div>
     )
 }
--- frontend/src/components/profile/TabBar.jsx
+++ frontend/src/components/profile/TabBar.jsx
@@ -1,11 +1,11 @@
 import { TabButton } from "./TabButton"
 
-export const TabBar = ({ setShowWall, setShowInfo }) => {
+export const TabBar = ({ setShowWall, setShowInfo, setShowPhotos }) => {
     return (
         <div className="TabBar">
-            <TabButton name="wall" value="wall" setShowComponent={setShowWall} otherComponents={[setShowInfo]}/>
-            <TabButton name="info" value="info" setShowComponent={setShowInfo} otherComponents={[setShowWall]} />
-            <TabButton name="photos" value="photos" setShowWall={setShowWall} />
+            <TabButton name="wall" value="wall" setShowComponent={setShowWall} otherComponents={[setShowInfo, setShowPhotos]}/>
+            <TabButton name="info" value="info" setShowComponent={setShowInfo} otherComponents={[setShowWall, setShowPhotos]} />
+            <TabButton name="photos" value="photos" setShowComponent={setShowPhotos} otherComponents={[setShowWall, setShowInfo]} />
             <TabButton name="prods" value="prods" setShowWall={setShowWall} />
             <TabButton name="friends" value="friends" setShowWall={setShowWall} />
         </div>
