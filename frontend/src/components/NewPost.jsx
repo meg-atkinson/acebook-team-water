021fdfe5085e7ac376cdf207e0937918de29487a
Olly <ollywaldron11@yahoo.co.uk>
2025-06-04 14:20:13 +0100

    feed page adding new posts

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,17 +1,45 @@
 import { useState } from 'react';
 
-const NewPost = () => {
+const NewPost = ({ onPostCreated }) => {
 
     const [newPost, setNewPost] = useState("");
 
     
-    const handleSubmit = (event) => {
-    event.default()
-    fetch("http://url.com/newpost", {
-        method: "POST",
-        body: JSON.stringify({ newPost: newPost }),
-    });
-}
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault()
+    //     const token = localStorage.getItem("token");
+    //     const response = await fetch("http://url.com/newpost", {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${token}`,
+    //         },
+    //         body: JSON.stringify({ newPost: newPost }),
+    // });
+
+    //     if (response.ok) {
+    //         const createdPost = await response.json();
+    //         setNewPost("");
+    //         if (onPostCreated) {
+    //             onPostCreated(createdPost);
+    //         }
+    //     }
+    // };
+
+    const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Simulate a fake "post creation"
+    const fakePost = {
+        _id: Date.now().toString(),
+        message: newPost,
+    };
+
+    setNewPost("");
+    if (onPostCreated) {
+        onPostCreated(fakePost); // pretend it was saved
+    }
+};
 
     return (
         <div>
@@ -25,7 +53,6 @@ const NewPost = () => {
                 <br />
                 <br />
                 <label>
-            
                     <input type="submit" value="Post" />
                 </label>
             </form>
