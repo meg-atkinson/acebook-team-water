99ae84511ea9a2c801d614fa7b765603c848c7ec
evemoore <eveiaim98@outlook.com>
2025-06-06 16:28:13 +0100

    Changes

--- frontend/src/components/profile/Status.jsx
+++ frontend/src/components/profile/Status.jsx
@@ -67,7 +67,7 @@ export const Status = () => {
             }
         };
         fetchPostsById();
-    }, []);
+    }, [updateStatus]);
 
 
     // ------------------------------- creating a new status ------------------------------
