6870a82249402d03d6e1ffd4ac55261b61151c44
evemoore <eveiaim98@outlook.com>
2025-06-06 13:45:51 +0100

    Update status adds to post, with postType: status, to db

--- frontend/src/components/profile/NewStatus.jsx
+++ frontend/src/components/profile/NewStatus.jsx
@@ -1,11 +1,11 @@
-export const NewStatus = ({ handleSubmit, handleChange, handleClick }) => {
+export const NewStatus = ({ handleSubmit, newStatus, handleChange, handleClick }) => {
 
     return (
         <div className="statusContainer">
                 <form onSubmit={handleSubmit}>
-                    <input placeholder="Write your new status here" />
+                    <input name="content" value={newStatus.content} onChange={handleChange} placeholder="Write your new status here" />
                     <div className="updateCancelButttons">
-                        <input type="submit" value="Update" onChange={handleChange} />
+                        <input type="submit" value="Update" />
                         <button onClick={handleClick}>Cancel</button>
                     </div>
                 </form>
--- frontend/src/components/profile/Status.jsx
+++ frontend/src/components/profile/Status.jsx
@@ -1,27 +1,71 @@
 import { useState } from "react"
+import { useParams } from 'react-router-dom'
 import { NewStatus } from "./NewStatus"
 
 export const Status = () => {
+    // Boolean to conditionally render status || new status form
     const [updateStatus, setUpdateStatus] = useState(false)
-    const [newStatus, setNewStatus] = useState('')
 
     const handleClick = () => {
         setUpdateStatus(!updateStatus)
         console.log(updateStatus)
     }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        fetch("http://url.com/newstatus", {
-            method: "POST",
-            body: JSON.stringify({ newStatus: newStatus })
-        });
-    }
+    // ^^
+
+    // Get userID from url
+    const { userID: userIDFromURL } = useParams();
+
+    const [newStatus, setNewStatus] = useState({
+        userID: userIDFromURL,
+        content: "",
+        postType: "status",
+        targetUserID: userIDFromURL
+    })
+
+    
+
 
     const handleChange = (event) => {
-        setNewStatus(event.target.value)
-        setUpdateStatus(!updateStatus)
+        setNewStatus((prevStatus) => ({
+            ...prevStatus,
+            postType: "status",
+            [event.target.name]: event.target.value
+        }));
     }
+
+
+
+    
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const token = localStorage.getItem("token")
+
+        try {
+            const response = await fetch("http://localhost:3000/posts", {
+                method: "POST",
+                headers: { 
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+                body: JSON.stringify(newStatus)
+            })
+            setUpdateStatus(!updateStatus);
+
+            if (!response.ok) {
+                const errorMessage = await response.json();
+                throw new Error(errorMessage || `Signup fail: ${response.status}`);
+            }
+        } catch (error) {
+            console.error("Error in updating status:", error.message)
+        }
+    };
+
+    // const handleChange = (event) => {
+    //     setNewStatus(event.target.value)
+    //     setUpdateStatus(!updateStatus)
+    // }
 
     if (!updateStatus) {
         return (
@@ -36,6 +80,6 @@ export const Status = () => {
             </div>
         )
     } else {
-        return <NewStatus handleClick={handleClick} handleChange={handleChange} handleSubmit={handleSubmit}/>
+        return <NewStatus handleClick={handleClick} newStatus={newStatus} handleChange={handleChange} handleSubmit={handleSubmit}/>
     }
 }
