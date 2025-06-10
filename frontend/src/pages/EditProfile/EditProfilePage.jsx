455e740be703adb96f4addaf15759cb7834b5a90
Sarah Mathieson <sarahmathieson@nationwide.co.uk>
2025-06-10 15:57:57 +0100

    EPP-changes

--- frontend/src/pages/EditProfile/EditProfilePage.jsx
+++ frontend/src/pages/EditProfile/EditProfilePage.jsx
@@ -5,7 +5,7 @@ import './EditProfilePage.css';
 
 import Navbar from "../../components/navbar";
 import { SideProfile } from "../../components/profile/SideColumn";
-import { Info } from "../../components/profile/Info";
+import { UpdateOtherInfo } from "../../components/updateOtherInfo";
 import { getUser } from "../../services/user";
 
 export const EditProfilePage = () => {
@@ -13,15 +13,7 @@ export const EditProfilePage = () => {
     const location = useLocation();
     const [user, setUser] = useState(null)
     const navigate = useNavigate();
-
-        // Debug logging
-    console.log("=== DEBUG INFO ===");
-    console.log("Current URL:", window.location.href);
-    console.log("Location pathname:", location.pathname);
-    console.log("useParams() result:", useParams());
-    console.log("ID from useParams:", id);
-    console.log("ID type:", typeof id);
-    console.log("==================");
+    
 
     useEffect(() => {
         const token = localStorage.getItem("token");
@@ -35,7 +27,6 @@ export const EditProfilePage = () => {
         const fetchUserProfile = async () => {
             try{
                 if(!id){
-                    console.log("No ID found, calling getMe");
                     const meRes = await getMe(token)
                     setUser(meData)
                     navigate(`/editprofile/${meData._id}`, {replace: true});
@@ -43,7 +34,6 @@ export const EditProfilePage = () => {
             }
 
         // if not logged in user, get friends profile based on url
-                console.log("About to call getUser with ID:", id);
                 const userData = await getUser(token, id)
                 setUser(userData.user)
                 console.log(userData.user)
@@ -79,7 +69,7 @@ return (
                 ) : (
                     <p>Loading user info...</p>
                 )}
-                <Info user={user} />
+                <UpdateOtherInfo user={user} />
             </div>
         </>
     );
