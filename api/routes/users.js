67d8fe972a0ccf26c6705952504220d1cb890de0
Meg Atkinson <megan.e.atkinson@gmail.com>
2025-06-11 11:55:42 +0200

    authentication routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { create, uploadMiddleware } = require('../controllers/users');
 const upload = require('../middleware/upload');
+const tokenChecker = require("../middleware/tokenChecker");
 
 const UsersController = require("../controllers/users");
 
 const router = express.Router();
 
-router.get("/", UsersController.getAllUsers);
+router.get("/", tokenChecker, UsersController.getAllUsers);
 
-router.get("/me", UsersController.getCurrentUser)
+router.get("/me", tokenChecker, UsersController.getCurrentUser)
 
-router.get("/:id", UsersController.getUserByID);
+router.get("/:id", tokenChecker, UsersController.getUserByID);
 
 router.post('/', upload.single('profilePicture'), UsersController.create);
 
